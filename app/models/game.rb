1895cd6e2c3696a8efecd77237df8c59d8457d27
ttovarischh <polinasot@gmail.com>
2023-06-19 20:33:38 +0300

    Last bug fix

--- app/models/game.rb
+++ app/models/game.rb
@@ -2,30 +2,34 @@ class Game < ApplicationRecord
     validates_uniqueness_of :id
     has_many :players, dependent: :destroy
     has_many :monsters, dependent: :destroy
-    belongs_to :user
+    belongs_to :user 
 
     after_update do
-        puts "After update hook triggered"
-        if saved_change_to_fight?
-          puts "The 'fight' attribute is being changed"
-          if !fight
-            players.each do |player|
-              player.update(initiative: 0, active: false)
-            end
-            puts "Initiatives of players have been reset to 0"
-                  
-            monsters.each do |monster|
-              monster.destroy
-            end
-            puts "Monsters have been destroyed"
-            
-            update(turn: 0)
-            puts "The 'turn' attribute has been set to 0"
-          end
+      puts "After update hook triggered"
+      if saved_change_to_fight?
+        puts "The 'fight' attribute is being changed"
+        if fight
+          update(turn: 1)
+          puts "The 'turn' attribute has been set to 1"
         else
-          puts "The 'fight' attribute is not being changed"
+          players.each do |player|
+            player.update(initiative: 0, active: false)
+          end
+          puts "Initiatives of players have been reset to 0"
+          
+          monsters.each do |monster|
+            monster.destroy
+          end
+          puts "Monsters have been destroyed"
+
+          update(turn: 0)
+          puts "The 'turn' attribute has been set to 0"
         end
+      else
+        puts "The 'fight' attribute is not being changed"
+      end
     end
+    
       
     def to_param
         code.parameterize
